bbf3bad5cc0f794093a7ebe97f83902dcd3ee872
jirigav <jirigavenda98@gmail.com>
2022-03-19 16:17:15 +0100

    process sign

--- server-signer/src/requests.rs
+++ server-signer/src/requests.rs
@@ -5,7 +5,10 @@ use crate::key_gen::{GG18KeyGenContext1, GG18KeyGenContext2, GG18KeyGenContext3,
     gg18_key_gen_4, gg18_key_gen_5, gg18_key_gen_6,
     GG18KeyGenMsg1, GG18KeyGenMsg2, GG18KeyGenMsg3, GG18KeyGenMsg4, GG18KeyGenMsg5};
 use crate::sign::{GG18SignContext1, GG18SignContext2, GG18SignContext3, GG18SignContext4,
-    GG18SignContext5, GG18SignContext6, GG18SignContext7, GG18SignContext8, GG18SignContext9};
+    GG18SignContext5, GG18SignContext6, GG18SignContext7, GG18SignContext8, GG18SignContext9,
+    gg18_sign1, gg18_sign2, gg18_sign3, gg18_sign4, gg18_sign5, gg18_sign6, gg18_sign7, gg18_sign8,
+    gg18_sign9, gg18_sign10, GG18SignMsg1, GG18SignMsg2, GG18SignMsg3, GG18SignMsg4, GG18SignMsg5,
+    GG18SignMsg6, GG18SignMsg7, GG18SignMsg8, GG18SignMsg9};
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 enum RequestType {
@@ -138,8 +141,108 @@ pub fn process_request(context: &Context, request_buf: Vec<u8>) -> (Context, Res
                                             .map(| x | serde_json::from_slice(&x).unwrap())
                                             .collect::<Vec<GG18KeyGenMsg5>>();
                 let c = gg18_key_gen_6(messages, context).unwrap();
+                fs::write(SIGNCONTEXTPATH, serde_json::to_string(&c).unwrap()).expect("Unable to save setup file.");
+
                 (Context::Empty, Response{ response_type: ResponseType::GenerateKey,
                                                     data: vec!(serde_json::to_vec(&c.pk).unwrap())})
+            }
+
+        (Context::SignContext0(context), RequestType::Sign) =>
+            {
+                let (m, c) = gg18_sign1(context, request.data[0].clone().into_iter().map(|x| x as u16).collect()
+                , request.data[1].clone()).unwrap();
+                (Context::SignContext1(c), Response{ response_type: ResponseType::GenerateKey,
+                                                    data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+
+        (Context::SignContext1(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg1>>();
+                let (m, c) = gg18_sign2(messages, context).unwrap();
+                (Context::SignContext2(c), Response{ response_type: ResponseType::Sign,
+                                                     data: m.into_iter()
+                                                            .map(|x| serde_json::to_vec(&x).unwrap())
+                                                            .collect()})
+            }
+
+        (Context::SignContext2(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg2>>();
+                let (m, c) = gg18_sign3(messages, context).unwrap();
+                (Context::SignContext3(c), Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+
+        (Context::SignContext3(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg3>>();
+                let (m, c) = gg18_sign4(messages, context).unwrap();
+                (Context::SignContext4(c), Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+        (Context::SignContext4(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg4>>();
+                let (m, c) = gg18_sign5(messages, context).unwrap();
+                (Context::SignContext5(c), Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+        (Context::SignContext5(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg5>>();
+                let (m, c) = gg18_sign6(messages, context).unwrap();
+                (Context::SignContext6(c), Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+
+        (Context::SignContext6(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg6>>();
+                let (m, c) = gg18_sign7(messages, context).unwrap();
+                (Context::SignContext7(c), Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+
+        (Context::SignContext7(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg7>>();
+                let (m, c) = gg18_sign8(messages, context).unwrap();
+                (Context::SignContext8(c), Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+
+        (Context::SignContext8(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg8>>();
+                let (m, c) = gg18_sign9(messages, context).unwrap();
+                (Context::SignContext9(c), Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(serde_json::to_vec(&m).unwrap())})
+            }
+
+        (Context::SignContext9(context), RequestType::Sign) =>
+            {
+                let messages = request.data.into_iter()
+                                            .map(| x | serde_json::from_slice(&x).unwrap())
+                                            .collect::<Vec<GG18SignMsg9>>();
+                let s = gg18_sign10(messages, context).unwrap();
+                (Context::Empty, Response{ response_type: ResponseType::GenerateKey,
+                                                     data: vec!(s)})
             }
 
         _ => {let response = b"Ok\n";
